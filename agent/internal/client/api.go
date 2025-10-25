45c539cfa57a80ff9a22bb1c40ebedac8266abba
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-10-25 18:25:53 +0200

    feat: Add NewAPIClient function to initialize APIClient with timeout

--- agent/internal/client/api.go
+++ agent/internal/client/api.go
@@ -1,9 +1,22 @@
 package client
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type APIClient struct {
 	ServerUrl string
 	AgentID   string
 	client    *http.Client
+}
+
+func NewAPIClient(serverURL string, agentID string) *APIClient {
+	return &APIClient{
+		ServerUrl: serverURL,
+		AgentID:   agentID,
+		client: &http.Client{
+			Timeout: 30 * time.Second,
+		},
+	}
 }
